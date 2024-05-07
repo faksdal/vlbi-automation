db65105d83a9ff4b09f2a6b112bc62925918d441
Jon Leithe <jon@jonleithe.no>
2024-05-07 14:45:37 +0200

    Added class for file operations

---
+++ inc/FileOperations.h
@@ -0,0 +1,24 @@
+/*
+ * FileOperations.h
+ *
+ *  Created on: May 7, 2024
+ *      Author: leijon
+ */
+
+#ifndef INC_FILEOPERATIONS_H_
+#define INC_FILEOPERATIONS_H_
+
+namespace std {
+
+/*
+ *
+ */
+class FileOperations {
+public:
+	FileOperations();
+	virtual ~FileOperations();
+};
+
+} /* namespace std */
+
+#endif /* INC_FILEOPERATIONS_H_ */
---
+++ src/FileOperations.cpp
@@ -0,0 +1,21 @@
+/*
+ * FileOperations.cpp
+ *
+ *  Created on: May 7, 2024
+ *      Author: leijon
+ */
+
+#include "/home/leijon/projects/vlbi-automation/inc/FileOperations.h"
+
+namespace std {
+
+FileOperations::FileOperations() {
+	// TODO Auto-generated constructor stub
+
+}
+
+FileOperations::~FileOperations() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
